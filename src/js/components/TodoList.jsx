5c7a79b882948da0d3003c3a4c006358491e214c
Daniela Maica <danielamaica43@gmail.com>
2025-09-16 20:46:57 +0000

    Proceso

---
+++ src/js/components/TodoList.jsx
@@ -0,0 +1,21 @@
+
+const TodoList = () => {
+    return(
+        <>
+        <h1 className="text-center">todos</h1>
+        <div className="container ">
+            <form>
+                <input type="text" placeholder="What needs to be done?"/>
+                <ul>
+                    <li>hoal </li>
+                    <li>hoal </li>
+                    <li>hopal </li>
+                    <li>hgola </li>
+                </ul>
+            </form>
+        </div>
+        </>
+    );
+};
+
+export default TodoList;
