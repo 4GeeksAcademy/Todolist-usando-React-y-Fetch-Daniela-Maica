c4c2b4655e7bd281ca6381d4de0630c631264b02
Daniela Maica <danielamaica43@gmail.com>
2025-09-17 01:20:47 +0000

    Diseño casi listo

--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -3,15 +3,16 @@ const TodoList = () => {
     return(
         <>
         <h1 className="text-center">todos</h1>
-        <div className="container ">
+        <div className="list">
             <form>
                 <input type="text" placeholder="What needs to be done?"/>
                 <ul>
-                    <li>hoal </li>
-                    <li>hoal </li>
-                    <li>hopal </li>
-                    <li>hgola </li>
+                    <li>Wola</li>
+                    <li>Wola</li>
+                    <li>Wola</li>
+                    <li>Wola</li>
                 </ul>
+                <div className="footer">5 item left</div>
             </form>
         </div>
         </>
